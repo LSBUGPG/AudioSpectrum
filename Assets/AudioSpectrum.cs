8875221d70962ce19fe60a5bbb5e93f202cfe308
Paul Sinnett <paul.sinnett@gmail.com>
2018-11-27 15:30:56 +0000

    Initial commit

---
+++ Assets/AudioSpectrum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSpectrum : MonoBehaviour {
+
+	public Slider barPrefab;
+	Slider [] bars;
+
+	void Start()
+	{
+		bars = new Slider[16];
+		for (int i = 0; i < bars.Length; ++i) {
+			Slider bar = Instantiate (barPrefab, transform);
+			bar.transform.localPosition = new Vector3 ((i - 8) * 20.0f, 0.0f, 0.0f);
+			bars [i] = bar;
+		}
+	}
+
+	void Update()
+	{
+		float[] spectrum = new float[256];
+
+		AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+		for (int i = 0; i < bars.Length; ++i) {
+			bars [i].value = spectrum [i];
+		}
+	}
+}
